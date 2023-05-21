9a66e15242a241597c90e51780bb7eb2a320db98
LuisEnrique25 <luisrg2225@gmail.com>
2023-05-21 13:27:42 -0600

    Agregue la funcion para obtener un elemento aleatorio del json y se la pase al hook useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import './App.css'
+import { useState } from 'react'
+import dbSartans from "./db/spartans.json"
+import { getRandom } from './utils/random.js'
 
 function App() {
-
+  const [spartan, setSpartan] = useState(getRandom(dbSartans))
+  console.log(spartan)
   return (
     <main>
 
