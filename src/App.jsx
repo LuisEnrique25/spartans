b834fa37095a74fb1c2bbf7d4fbbf28a4ac952b1
LuisEnrique25 <luisrg2225@gmail.com>
2023-05-21 14:55:08 -0600

    Al elemento main le agregue el className de app para su uso en App.css

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,22 @@ import './App.css'
 import { useState } from 'react'
 import dbSartans from "./db/spartans.json"
 import { getRandom } from './utils/random.js'
+import SpartanBox from './components/SpartanBox'
 
 function App() {
   const [spartan, setSpartan] = useState(getRandom(dbSartans))
-  console.log(spartan)
+  const handleChangeSpartan = () =>{
+    setSpartan(getRandom(dbSartans));
+  }
   return (
-    <main>
+    <main className='app bg10'>
+      <section>
+          <h1>SPARTANS</h1>
+        </section>
+        <SpartanBox spartan={spartan} handleChangeSpartan={handleChangeSpartan} />
+        <footer></footer>
 
+        
     </main>
   )
 }
