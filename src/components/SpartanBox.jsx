d6b82dd836e162eaf29eacbeefaa1d3ce812740d
LuisEnrique25 <luisrg2225@gmail.com>
2023-05-23 16:00:31 -0600

    Prueba con la ruta de img en el archivo json y agregue contenido al atributo alt del elemnto img

--- src/components/SpartanBox.jsx
+++ src/components/SpartanBox.jsx
@@ -8,7 +8,7 @@ const SpartanBox = ({spartan, handleChangeSpartan}) => {
         <article className="card">
           
             <div className="face front">
-                <img src={`${spartan.img}`} alt="Noble 3"/>
+                <img src={`${spartan.img}`} alt={`${spartan.name}`}/>
                 <h3>{`${spartan.name}`}</h3>
             </div>
             <div className="face back">
